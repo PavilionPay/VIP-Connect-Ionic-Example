cf443441f297b9d51fd1fc8e66c422b68c68d157
James Wright <james.wright@pavilionpay.com>
2025-02-07 13:12:18 -0500

    Resolved conflict

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,16 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
 import { environment } from 'src/environments/environment';
+import { Platform } from '@ionic/angular';
+import { registerPlugin } from '@capacitor/core';
+
+export interface VIPCPlugin {
+    start(options: { url: string }): Promise<{ value: string }>;
+}
+
+const VIPCPlugin = registerPlugin<VIPCPlugin>('VIPCPlugin');
+
+export default VIPCPlugin;
 
 @Component({
   selector: 'app-home',
@@ -9,9 +19,24 @@ import { environment } from 'src/environments/environment';
   imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton ],
 })
 export class HomePage {
-  constructor() {}
+  constructor(public platform: Platform) {}
 
   async launchVIPC() {
-    window.open(`${environment.vipc}/sdk?mode=deposit#${environment.devTestOperator.sessionId}`, '_blank');
+    let url = `${environment.vipc}/sdk?mode=deposit#${environment.devTestOperator.sessionId}`;
+
+    if (this.platform.is('android')) { 
+      // await InAppBrowser.openInSystemBrowser({ 
+      //   url:'https://qa.api-gaming.paviliononline.io/sdk?mode=deposit#0502bf21-32bd-49e4-8f2c-e7bec67c7676',
+      //   options: DefaultSystemBrowserOptions
+      // });
+      // await InAppBrowser.addListener('browserClosed', () => {
+      //   console.log('Browser closed');
+      // });
+    }
+
+    if (this.platform.is('ios')) {
+      const { value } = await VIPCPlugin.start({ url: 'https://qa.api-gaming.paviliononline.io/sdk?mode=deposit#1d9cd474-75a0-4220-9704-761261afe988' }); 
+      console.log(value);
+    }
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: false,
+  vipc: 'https://dev.api-gaming.paviliononline.io',
+  devTestOperator: {
+    sessionId: 'aa3dd019-495e-433d-8cab-a0e3ac275882' // Use session-id here
+  }
 };
